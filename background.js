b434699494c3e790212cce0f0663ce2e98e1d341
Deep Datta Roy <deepdattaroy8888@gmail.com>
2013-06-10 22:44:03 -0400

    Removed Extra Files

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(
-        function(tab) {
-        	chrome.tabs.create({url:'http://www.bing.com/search?q=asdasd'});
-            chrome.tabs.executeScript(tab.id, {file: 'rewards.js'})
-        }
-);
