3fb79d313c8ce7547373e1ffc2c5b8c3ee25a4fd
Deep Datta Roy <deepdattaroy8888@gmail.com>
2013-06-12 22:10:14 -0400

    Got rid of some remenet code

--- rewards.js
+++ rewards.js
@@ -84,8 +84,8 @@ function onTrendData() {
 	} else if(req1.readyState == 4 && index[1]==0) {
 		data = JSON.parse(req1.responseText);
 		index[1] = 1;
-		for (var k = 4; k < 8; k++) {
-           	stringArr = data ["responseData"]["results"][k-4]["title"];
+		for (var k = 0; k < 4; k++) {
+           	stringArr = data ["responseData"]["results"][k]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
 			stringArr =stringArr.replace("<b>","");
 			stringArr =stringArr.replace("</b>","");
@@ -94,38 +94,38 @@ function onTrendData() {
 	} else if(req2.readyState == 4 && index[2]==0) {
 		data = JSON.parse(req2.responseText);
 		index[2] = 1;
-		for (var k = 8; k < 12; k++) {
-            stringArr = data ["responseData"]["results"][k-8]["title"];
+		for (var k = 0; k < 4; k++) {
+           	stringArr = data ["responseData"]["results"][k]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
 			stringArr =stringArr.replace("<b>","");
 			stringArr =stringArr.replace("</b>","");
 			chrome.tabs.create({"url":"http://www.bing.com/search?q=" + stringArr, selected: true, active: false});
-  		}
+   		}
 	} else if(req3.readyState == 4 && index[3]==0) {
 		data = JSON.parse(req3.responseText);
 		index[3] = 1;
-		for (var k = 12; k < 16; k++) {
- 			stringArr = data ["responseData"]["results"][k-12]["title"];
+		for (var k = 0; k < 4; k++) {
+           	stringArr = data ["responseData"]["results"][k]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
 			stringArr =stringArr.replace("<b>","");
 			stringArr =stringArr.replace("</b>","");
 			chrome.tabs.create({"url":"http://www.bing.com/search?q=" + stringArr, selected: true, active: false});
-  		}
+   		}
 	} else if(req4.readyState == 4 && index[4]==0) {
 		data = JSON.parse(req4.responseText);
 		index[4] = 1;
-		for (var k = 16; k < 20; k++) {
-   			stringArr = data ["responseData"]["results"][k-16]["title"];
+		for (var k = 0; k < 4; k++) {
+           	stringArr = data ["responseData"]["results"][k]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
 			stringArr =stringArr.replace("<b>","");
 			stringArr =stringArr.replace("</b>","");
 			chrome.tabs.create({"url":"http://www.bing.com/search?q=" + stringArr, selected: true, active: false});
-  		}
+   		}
 	} else if(req5.readyState == 4 && index[5]==0) {
 		data = JSON.parse(req5.responseText);
 		index[5] = 1;
-		for (var k = 20; k < 24; k++) {
-   			stringArr = data ["responseData"]["results"][k-20]["title"];
+		for (var k = 0; k < 4; k++) {
+           	stringArr = data ["responseData"]["results"][k]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
 			stringArr =stringArr.replace("<b>","");
 			stringArr =stringArr.replace("</b>","");
