48564244c03d8889b51ab4d51148afb18529044c
Deep Datta Roy <deepdattaroy8888@gmail.com>
2013-06-12 22:06:58 -0400

    Fixed some code with getting rid of tags

--- rewards.js
+++ rewards.js
@@ -73,10 +73,12 @@ function onTrendData() {
 	//Get data from each trend when ready
 	if(req0.readyState == 4 && index[0]==0) {
 		data = JSON.parse(req0.responseText);
-		index[1] = 1;
+		index[0] = 1;
 		for (var k = 0; k < 4; k++) {
            	stringArr = data ["responseData"]["results"][k]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
+			stringArr =stringArr.replace("<b>","");
+			stringArr =stringArr.replace("</b>","");
 			chrome.tabs.create({"url":"http://www.bing.com/search?q=" + stringArr, selected: true, active: false});
    		}
 	} else if(req1.readyState == 4 && index[1]==0) {
@@ -85,6 +87,8 @@ function onTrendData() {
 		for (var k = 4; k < 8; k++) {
            	stringArr = data ["responseData"]["results"][k-4]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
+			stringArr =stringArr.replace("<b>","");
+			stringArr =stringArr.replace("</b>","");
 			chrome.tabs.create({"url":"http://www.bing.com/search?q=" + stringArr, selected: true, active: false});
    		}
 	} else if(req2.readyState == 4 && index[2]==0) {
@@ -93,6 +97,8 @@ function onTrendData() {
 		for (var k = 8; k < 12; k++) {
             stringArr = data ["responseData"]["results"][k-8]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
+			stringArr =stringArr.replace("<b>","");
+			stringArr =stringArr.replace("</b>","");
 			chrome.tabs.create({"url":"http://www.bing.com/search?q=" + stringArr, selected: true, active: false});
   		}
 	} else if(req3.readyState == 4 && index[3]==0) {
@@ -101,6 +107,8 @@ function onTrendData() {
 		for (var k = 12; k < 16; k++) {
  			stringArr = data ["responseData"]["results"][k-12]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
+			stringArr =stringArr.replace("<b>","");
+			stringArr =stringArr.replace("</b>","");
 			chrome.tabs.create({"url":"http://www.bing.com/search?q=" + stringArr, selected: true, active: false});
   		}
 	} else if(req4.readyState == 4 && index[4]==0) {
@@ -109,6 +117,8 @@ function onTrendData() {
 		for (var k = 16; k < 20; k++) {
    			stringArr = data ["responseData"]["results"][k-16]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
+			stringArr =stringArr.replace("<b>","");
+			stringArr =stringArr.replace("</b>","");
 			chrome.tabs.create({"url":"http://www.bing.com/search?q=" + stringArr, selected: true, active: false});
   		}
 	} else if(req5.readyState == 4 && index[5]==0) {
@@ -117,6 +127,8 @@ function onTrendData() {
 		for (var k = 20; k < 24; k++) {
    			stringArr = data ["responseData"]["results"][k-20]["title"];
 			stringArr =stringArr.replace(/#/gi,"");
+			stringArr =stringArr.replace("<b>","");
+			stringArr =stringArr.replace("</b>","");
 			chrome.tabs.create({"url":"http://www.bing.com/search?q=" + stringArr, selected: true, active: false});
    		}
 	}	
