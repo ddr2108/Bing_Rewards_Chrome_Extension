218ca574c373faae80eac8d816eae4152cf20910
ddr2108 <deepdattaroy8888@gmail.com>
2013-12-13 12:31:21 -0500

    Added 1s between tabs

--- rewards.js
+++ rewards.js
@@ -74,43 +74,35 @@ function onTrendData() {
 	//Get data from each trend when ready
 	if(req0.readyState == 4 && index[0]==0) {
 		index[0] = 1;			//Set index bit as read
-		search(req0);			//Do the actual searches
+		//search(req0);			//Do the actual searches
 	} else if(req1.readyState == 4 && index[1]==0) {
 		index[1] = 1;			//Set index bit as read
-		search(req1);			//Do the actual searches
+		//search(req1);			//Do the actual searches
 	} else if(req2.readyState == 4 && index[2]==0) {
 		index[2] = 1;			//Set index bit as read
-		search(req2);			//Do the actual searches
+		//search(req2);			//Do the actual searches
 	} else if(req3.readyState == 4 && index[3]==0) {
 		index[3] = 1;			//Set index bit as read
-		search(req3);			//Do the actual searches
+		//search(req3);			//Do the actual searches
 	} else if(req4.readyState == 4 && index[4]==0) {
 		index[4] = 1;			//Set index bit as read
-		search(req4);			//Do the actual searches
+		//search(req4);			//Do the actual searches
 	} else if(req5.readyState == 4 && index[5]==0) {
 		index[5] = 1;			//Set index bit as read
-		search(req5);			//Do the actual searches
+		//search(req5);			//Do the actual searches
 	}	
+
+	if (index[0]==1 && index[1]==1 && index[2]==1 && index[3]==1 && index[4]==1 && index[5]==1){
+		bing();
+	}
 }
 
-//Do the actual searches
-function search(request){
-	data = JSON.parse(request.responseText);							//Parse data
-	for (var k = 0; k < 4; k++) {
-       	stringArr = data ["responseData"]["results"][k]["title"];		//Get the title
-		stringArr = stringArr.replace(/#/gi,"");						//Clean up string
-		stringArr = stringArr.replace("<b>","");
-		stringArr = stringArr.replace("</b>","");
-   	}
-   	        setTimeout(bing, 2000);
-
-}
 
 function bing(){
 	i++;
-	if (i<4){
+	if (i<40){
 		chrome.tabs.create({"url":"http://www.bing.com/search?q=" + "1a", selected: true, active: false});		//Do the search
-        setTimeout(bing, 2000);
+        setTimeout(bing, 1000);
 	}else{
 		i =0;
 	}
